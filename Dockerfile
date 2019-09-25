6d5b35ab64536b26cbc66855be2edf44e89a77bc
Faisal Alam <faisal.alam@rentomojo.com>
2019-09-26 01:17:37 +0530

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM php:7.2-cli
+
+COPY . /usr/src/myapp
+
+WORKDIR /usr/src/myapp
+
+CMD [ "php", "-S", "0.0.0.0:8000" ]
---
+++ upload_qr.php
@@ -0,0 +1,17 @@
+<?php
+
+function base64_to_image($base64_image) {
+    [$formatInfo, $data] = explode(",", $base64_image, 2);
+    $imageFormat = explode("/", explode(";", $formatInfo, 2)[0], 2)[1];
+
+    $imageFileName = "qr.${imageFormat}";
+
+    $fp = fopen($imageFileName, "wb");
+    fwrite($fp, base64_decode($data));
+    fclose($fp);
+
+    return $imageFileName;
+}
+
+$base64_image = @$_POST['base64_image'];
+base64_to_image($base64_image);
